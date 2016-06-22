47eb308ea2009d718dc29e06f0869ac9d394e4cb
Endre Midtgård Meckelborg <endre.midtgard.meckelborg@bring.com>
2016-06-22 13:31:00 +0200

    Minor improvements

--- src/no/bring/calculator/StringCalculator.java
+++ src/no/bring/calculator/StringCalculator.java
@@ -2,45 +2,38 @@ package no.bring.calculator;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 public class StringCalculator {
 
     private String delimiter = ",|\\n";
 
-    public int add(String numbers) {
-        if (isBlank(numbers)) {
+    public int add(String expression) {
+        if (expression.isEmpty()) {
             return 0;
         }
 
-        if (definesDelimiter(numbers)) {
-            String[] numberParts = numbers.split("\n");
-            delimiter = parseDelimiters(numberParts[0]);
-            return addNumbers(numberParts[1].split(delimiter));
+        if (containsDelimiter(expression)) {
+            String[] numberParts = expression.split("\n");
+            delimiter = parseDelimiter(numberParts[0]);
+            return summarize(numberParts[1].split(delimiter));
         }
 
-        return addNumbers(numbers.split(delimiter));
+        return summarize(expression.split(delimiter));
     }
 
-    private boolean isBlank(String numbers) {
-        return numbers == null || numbers.isEmpty();
-    }
-
-    private boolean definesDelimiter(String numbers) {
+    private boolean containsDelimiter(String numbers) {
         return numbers.startsWith("//");
     }
 
-    private String parseDelimiters(String delimiters) {
+    private String parseDelimiter(String delimiters) {
         return Arrays.stream(delimiters.substring(2).split("\\]\\["))
-            .map(this::parseDelimiter)
-            .collect(Collectors.joining("|"));
+            .map(d -> Pattern.quote(d.replace("[", "").replace("]", "")))
+            .collect(joining("|"));
     }
 
-    private String parseDelimiter(String delimiter) {
-        return Pattern.quote(delimiter.replace("[", "").replace("]", ""));
-    }
-
-    private int addNumbers(String[] numbers) {
+    private int summarize(String[] numbers) {
         assertNoNegatives(numbers);
         return Arrays.stream(numbers)
             .mapToInt(Integer::valueOf)
@@ -51,7 +44,7 @@ public class StringCalculator {
     private void assertNoNegatives(String[] numbers) {
         String negatives = Arrays.stream(numbers)
             .filter(n -> n.startsWith("-"))
-            .collect(Collectors.joining(","));
+            .collect(joining(","));
         if (!negatives.isEmpty()) {
             throw new IllegalArgumentException("Negatives not allowed: " + negatives);
         }
